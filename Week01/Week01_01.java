0acabd616bf80eff7c5fb43c0174943209a512a5
CodingKingDoyun <codingkingdoyun@gmail.com>
2026-03-14 22:00:18 +0900

    delete Week01, Week02

--- Week01/Week01_01.java
+++
@@ -1,8 +0,0 @@
-package Week01;
-
-public class Week01_01 {
-  public static void main(String[] arg){
-    System.out.println("Welcome!!");
-    System.out.println("Welcome to Java world.");
-  }
-}
--- Week01/Week01_02.java
+++
@@ -1,8 +0,0 @@
-package Week01;
-
-public class Week01_02 {
-  public static void main(String[] arg){
-    System.out.println("Sorry~~");
-    System.out.println("I'm sorry that it is so fun~~");
-  }
-}
